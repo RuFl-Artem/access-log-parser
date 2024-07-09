6b732bd9f92c25bac8b88917f9903a984f99dd22
Artem Melikhov <melcomeart@gmail.com>
2024-07-09 09:48:19 +0300

    Выполнение курсового проекта раздел 3. Задание по теме "Синтаксис языка" и “Базовые типы данных” Изменен метод Main, добавлен ввод двух чисел, вычисление и комментарии

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //ввод чисел пользователем в консоль
         System.out.print("Введите число №1 = ");
         int number1 = new Scanner(System.in).nextInt();
         System.out.print("Введите число №2 = ");
@@ -11,16 +12,19 @@ public class Main {
         System.out.println("Произведение чисел = " + multiply(number1, number2));
         System.out.println("Частное чисел = " + quotient(number1, number2));
     }
-
+    //метод для вычисления суммы двух чисел
     public static int sum(int x, int y) {
         return x + y;
     }
+    //метод для вычисления разности двух чисел
     public static int diff(int x, int y) {
         return x - y;
     }
+    //метод для вычисления произведения двух чисел
     public static int multiply(int x, int y) {
         return x * y;
     }
+    //частное двух чисел с приведением к типу double
     public static double quotient(int x, int y) {
         return (double) x / y;
     }
